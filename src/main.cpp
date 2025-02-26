12394cbc66f6ddfbed78fcc78ce1d205ff0b046a
echofrog <echofrog.dev@outlook.com>
2025-02-26 21:45:48 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,14 @@
 #include <fmt/core.h>
 #include <toml++/toml.hpp>
 
+#include "ftxui_wrapper.h"
+
 using namespace std::literals;
 
 int main()
 {
-    spdlog::info("Hello, this is spdlog!");
+    spdlog::error("This is an error log.");
+
     std::cout << fmt::format("Formatted output: {}\n", 42);
 	
 	// Create a TOML document from a string
@@ -17,20 +20,7 @@ int main()
         database = { server = "192.0.2.0", ports = [ 8001, 8001, 8002 ], connection_max = 5000, enabled = true }
     )";
 
-
-    auto config = toml::parse_file( "configuration.toml" );
-
-    // get key-value pairs
-    std::string_view library_name = config["library"]["name"].value_or(""sv);
-    std::string_view library_author = config["library"]["authors"][0].value_or(""sv);
-    int64_t depends_on_cpp_version = config["dependencies"]["cpp"].value_or(0);
-
-    // modify the data
-    config.insert_or_assign("alternatives", toml::array{
-        "cpptoml",
-        "toml11",
-        "Boost.TOML"
-    });
+    spdlog::info("Welcome to spdlog");
 
     return 0;
 }
