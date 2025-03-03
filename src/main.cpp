20c6f53d8fd141f756ce271cf487181c9528fbd2
echofrog <echofrog.dev@outlook.com>
2025-03-03 22:35:30 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,18 @@
-#include <iostream>
 #include <spdlog/spdlog.h>
 #include <fmt/core.h>
 #include <toml++/toml.hpp>
-#include <linenoise.h>
+#include <CLI/CLI.hpp>
+//---------------------------------------------------------------------------
 
-using namespace std::literals;
-
-void completionCallback(const char* input, linenoiseCompletions* completions) {
-    if (input[0] == 'h') {
-        linenoiseAddCompletion(completions, "hello");
-        linenoiseAddCompletion(completions, "help");
-    }
-}
-
-int main()
+int main(int argc, char** argv)
 {
-    spdlog::error("This is an error log.");
+    CLI::App app{"App description"};
+    argv = app.ensure_utf8(argv);
 
-    std::cout << fmt::format("Formatted output: {}\n", 42);
-	
-	// Create a TOML document from a string
-    const char* toml_data = R"(
-        title = "TOML Example"
-        owner = { name = "Tom Preston-Werner", dob = 1979-05-27 }
-        database = { server = "192.0.2.0", ports = [ 8001, 8001, 8002 ], connection_max = 5000, enabled = true }
-    )";
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
 
-    spdlog::info("Welcome to spdlog");
-
-    linenoiseSetMultiLine(1);
-    linenoiseSetCompletionCallback(completionCallback);
-    linenoiseHistoryLoad("history.txt");
-
-    char* input;
-    while ((input = linenoise(">>> ")) != nullptr) {
-        if (*input) {
-            linenoiseHistoryAdd(input);
-            linenoiseHistorySave("history.txt");
-        }
-        std::cout << "You entered: " << input << std::endl;
-        free(input);
-    }
-
+    CLI11_PARSE(app, argc, argv);
     return 0;
 }
+//---------------------------------------------------------------------------
